784d12f57565738c5b3012fb660c2a8c9d75630a
FAVIO VELEZ <favio.velez@hotmail.com>
2016-11-04 14:29:23 -0600

    exersise thirteen added

---
+++ nivel1/pregunta13.rb
@@ -0,0 +1,6 @@
+# Se tiene un array con los días feriados.
+# feriados = ["Lunes 19", "Martes 20","Miercoles 31"]
+# y se tiene una array con todos los días de clase del mes
+# dias = ["Lunes 3", "Martes 4" .. ]
+# 1) Suponiendo que el lunes cae el día tres, y este tiene 31 días. Generar el arreglo completo de días
+# 2) Crear un arreglo manual con 5 feriados
