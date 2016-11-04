a524b9c1f8ec5f9cc005ebf07f7a0771a28043f7
FAVIO VELEZ <favio.velez@hotmail.com>
2016-11-04 14:31:21 -0600

    exersise fifteen added

---
+++ nivel1/pregunta15.rb
@@ -0,0 +1,4 @@
+# Se tiene un arreglo de alumnos, cada alumno es un hash
+# alumnos = [{name:"Juan Cristobal", edad:17}, {name: "Belen", edad:21}]
+# Calcular la edad promedio del curso
+# Encontrar al alumno más joven del curso
