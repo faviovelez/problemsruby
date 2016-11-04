fd140cb799e5f1435ca74b859fc361e6cc437fd8
FAVIO VELEZ <favio.velez@hotmail.com>
2016-11-04 14:23:37 -0600

    sixth exersise

---
+++ nivel1/pregunta6.rb
@@ -0,0 +1,3 @@
+# Generar 20 números enteros al azar y guardarlos dentro de un arreglo, mostrar el arreglo y luego mostrar
+# del arreglo exclusivamente aquellos números que sean mayor que 10
+# http://ruby-doc.org/core-2.2.0/Random.html
