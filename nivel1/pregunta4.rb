bad0477d75827d8dba537478710490a11505cc1d
FAVIO VELEZ <favio.velez@hotmail.com>
2016-11-04 14:20:53 -0600

    fourth exersise

---
+++ nivel1/pregunta4.rb
@@ -0,0 +1,9 @@
+a = [1, 2, 3]
+b = [:azul, :rojo, :amarillo]
+c = ["Tacos", "Quesadillas", "Hamburguesas"]
+
+=begin
+Utilizando como base este arreglo: [1, :azul, Tacos, 2, :rojo, Quesadillas, 3,:amarillo, “Hamburguesas] 
+crear un arreglo nuevo remplazando todos los
+números y símbolos por string
+=end
