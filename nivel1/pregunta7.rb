a8efa955b2e35b3465c924b9e813b9a384ace383
FAVIO VELEZ <favio.velez@hotmail.com>
2016-11-07 19:21:05 -0600

    finished

--- nivel1/pregunta7.rb
+++ nivel1/pregunta7.rb
@@ -1,1 +1,12 @@
 # Generar 10 números enteros al azar y guardarlos dentro de un arreglo, luego calcular el promedio
+require 'pry'
+a = Array.new(10) { rand(1..25) }
+
+sum = 0
+b = (a.inject(0) do |sum, x| 
+	sum += x 	
+end)
+b /= (a.length).to_f
+puts b
+
+binding.pry
