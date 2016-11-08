bfb76d6f2be9a00005bdff6e4b8b45edfa3dd080
FAVIO VELEZ <favio.velez@hotmail.com>
2016-11-07 19:16:50 -0600

    finished

--- nivel1/pregunta5.rb
+++ nivel1/pregunta5.rb
@@ -3,3 +3,16 @@
 # Se pide cambiar todas las apariciones del número que aparece en el arreglo 1 por el elemento con el
 # mismo índice del arreglo 2, en caso de no existir el elemento deberá ser remplazado por nil.
 # El resultado de este ejercicio debería quedar:[:verde, :amarillo, nil, :azul, :verde, :amarillo ….]
+require 'pry'
+first = [1,2,3,0,1,2,2,1,2,1,2,0,3]
+second = [:azul, :verde, :amarillo]
+
+first.each_with_index do |i, n|
+	if first[n] <= second.length
+		first[n] = second[i]
+	else 
+		first[n] = nil
+	end
+end
+binding.pry
+# second.each_with_index do |n, i|
