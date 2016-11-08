a942b9ddc66b55eb7438b4348d813580ad179df1
FAVIO VELEZ <favio.velez@hotmail.com>
2016-11-07 19:11:13 -0600

    finished

--- nivel1/pregunta3.rb
+++ nivel1/pregunta3.rb
@@ -1,15 +1,17 @@
+require 'pry'
 a = [1, 2, 3]
 b = [:azul, :rojo, :amarillo]
 c = ["Tacos", "Quesadillas", "Hamburguesas"]
 
 =begin
 Iterando los arreglos anteriores crear un único arreglo final con: [1, :azul, Tacos, 2, :rojo, Quesadillas, 3,:amarillo, “Hamburguesas]
-:amarillo, “Hamburguesas]
 =end
 
-n = 0
+final = []
 
-(3.times do 
-	puts "#{a[n]} #{b[n]} #{c[n]}"
-	n += 1
-end).to_a
+3.times do |n|
+	final.push(a[n]) 
+	final.push(b[n]) 
+	final.push(c[n]) 
+end
+binding.pry
