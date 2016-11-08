e06247c4276670cab2f9e53fe7de567c81eb9088
FAVIO VELEZ <favio.velez@hotmail.com>
2016-11-08 17:57:37 -0600

    finished

--- nivel1/pregunta10.rb
+++ nivel1/pregunta10.rb
@@ -3,3 +3,13 @@
 # completamente nuevo, los elementos que no esten dentro de un arreglo serán ignorados. En el ejemplo
 # anterior el resultado quedaría:
 # [6,6]
+require 'pry'
+arr = [1,2,3,[1,2,3],4,5,6,[1,2,3]]
+arr_new = []
+
+for i in arr
+	if i.class == Array
+		arr_new.push(i.inject(0){|sum, x| sum += x })
+	end
+end
+binding.pry
