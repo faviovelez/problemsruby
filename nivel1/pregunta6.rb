a35a2c8b4bc8665eec9681e9c7d428b9ad305254
FAVIO VELEZ <favio.velez@hotmail.com>
2016-11-07 19:18:35 -0600

    finished

--- nivel1/pregunta6.rb
+++ nivel1/pregunta6.rb
@@ -1,3 +1,11 @@
 # Generar 20 números enteros al azar y guardarlos dentro de un arreglo, mostrar el arreglo y luego mostrar
 # del arreglo exclusivamente aquellos números que sean mayor que 10
 # http://ruby-doc.org/core-2.2.0/Random.html
+require 'pry'
+a = Array.new(20) { rand(1..25) }
+puts a
+b = (a.select do |x| 
+	x > 10 
+end)
+puts b
+binding.pry
